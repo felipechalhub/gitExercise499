8f395c406d1e9200293c7bbe39de1cfc4c5146b4
felipechalhub <fp.chalhub@gmail.com>
2021-05-18 16:51:27 -0300

    saving commen line

---
+++ src/sorting2.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class sorting2 {
+    public static void main(String[] args) {
+
+        int[] array2 = {4,65,6,5,6,72,5,2,3,56};
+        Arrays.sort(array2);
+        System.out.println(Arrays.toString(array2));
+        //this is a comment line
+    }
+}
