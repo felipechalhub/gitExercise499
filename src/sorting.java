ff4152a15c10588d1dfe149b0900bd4b6e69d3dc
felipechalhub <fp.chalhub@gmail.com>
2021-05-18 16:33:20 -0300

    testing sorting branch
    trying to use JUnit test with the .jar but failing

--- src/sorting.java
+++ src/sorting.java
@@ -1,16 +1,21 @@
-import java.util.Arrays;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.Arrays;
+import org.junit.Assert;
+import org.junit.Test;
 public class sorting {
     
         public static void main(String[] args) {
     
             int array[] = { 5, 1, 5, 61, 56, 7 };
-            int expected[] = { 1, 5, 5, 7, 56, 61 };
             selectionSort(array);
             System.out.println(Arrays.toString(array));
     
         }
-    
+        
         public static void selectionSort(int[] array) {
             for (int i = 0; i < array.length; i++) {
                 int min = array[i];
@@ -27,4 +32,26 @@ public class sorting {
                 array[minId] = temp;
             }
         }
-}
+        
+        
+        @Test
+        public void testEqual(){
+            int expected[] = { 1, 5, 5, 7, 56, 61 };
+            int array[] = { 5, 1, 5, 61, 56, 7 };
+            assertNotEquals(expected, array);
+            // Assert.assertEquals( expected, array );
+            // assertArrayEquals("They are equal", sorting.selectionSort(new int[] {5, 1, 5, 61, 56, 7 }), expected);
+        }
+        @Test
+        public void equals(){
+            long x = 5;
+            long y = 5;
+            Assert.assertEquals(x, y);
+            // assertEquals(x, y, "equal");
+            // assertEquals(x, y, "messageSupplier");
+            // assertEquals(x, y);
+            // assertEquals(x, y, "message");
+            // assertEquals(x, y, "messageSupplier");
+        }
+        
+    }
